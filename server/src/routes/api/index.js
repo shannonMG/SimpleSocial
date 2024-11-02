11769a12c02569efc6128d0a127b2bf974e9a032
shukikat <kathuriashuki@gmail.com>
2024-11-01 22:12:16 -0400

    removed js files

--- server/src/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/routes/api/index.ts
-var express_1 = require("express");
-var user_routes_1 = require("./user-routes");
-var router = express_1.default.Router();
-router.use('/users', user_routes_1.default); // This will handle routes at /api/users
-exports.default = router; // Export the router as the default export
